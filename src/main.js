f1b843d55ddace7d242f8d33d1b511b36ddd8b9d
Monse <m.pedraza.santoro@gmail.com>
2022-04-23 22:36:17 -0400

    Iniciando proyecto

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuSOe9OLyhlAlRefrtvTjcxzywabSYjZA",
+  authDomain: "cursos-alfaweb-ed1b7.firebaseapp.com",
+  projectId: "cursos-alfaweb-ed1b7",
+  storageBucket: "cursos-alfaweb-ed1b7.appspot.com",
+  messagingSenderId: "158991809607",
+  appId: "1:158991809607:web:eb67b95c7132dc9248c562"
+};
+
+const app = initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
